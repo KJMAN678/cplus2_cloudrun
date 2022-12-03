c841990b1196a19cede91d87848837bb9bd02187
KJMAN678 <kj.fireman5@gmail.com>
2022-12-03 18:55:34 +0900

    first commit

---
+++ cloud_run_hello.cc
@@ -0,0 +1,23 @@
+#include <google/cloud/functions/framework.h>
+#include <cstdlib>
+
+namespace gcf = ::google::cloud::functions;
+
+auto hello_world_http()
+{
+    return gcf::MakeFunction([](gcf::HttpRequest const & /*request*/)
+                             {
+    std::string greeting = "Hello ";
+    auto const* target = std::getenv("TARGET");
+    greeting += target == nullptr ? "World" : target;
+    greeting += "\n";
+
+    return gcf::HttpResponse{}
+        .set_header("Content-Type", "text/plain")
+        .set_payload(greeting); });
+}
+
+int main(int argc, char *argv[])
+{
+    return gcf::Run(argc, argv, hello_world_http());
+}
